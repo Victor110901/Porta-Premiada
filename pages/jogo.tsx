b5c567b04234f44eb443675ac9dca6d326f25572
victor.santos <victor.santos@pdcase.com>
2022-12-22 18:19:35 -0300

    Aula 91 next Criando botão para voltar a tela inicial

--- pages/jogo.tsx
+++ pages/jogo.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/Jogo.module.css"
 import { useState } from "react"
 import Porta from "../components/Porta"
 import { atualizarPortas, criarPortas } from "../functions/portas"
+import Link from "next/link"
 
 export default function jogo() {
     const [portas, setPortas] = useState(criarPortas(4, 3))
@@ -19,7 +20,9 @@ export default function jogo() {
           {renderizarPortas()}
         </div>
         <div className={styles.botoes}>
-
+            <Link href="/">
+                <button>Reiniciar Jogo</button>
+            </Link>
         </div>
     </div>
   )
