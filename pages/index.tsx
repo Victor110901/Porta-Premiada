2f21c7330cb673b23a27d4dcbc4162bd5a9b7322
victor.santos <victor.santos@pdcase.com>
2022-12-22 18:13:17 -0300

    Aula 90 next Criando pagina de jogo e ajuste no css

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,9 @@ import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta}
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
-
   return (
     <div style={{display: "flex"}}>
-      {renderizarPortas()}
+      <h1>Formulário de Inicio!!!</h1>
     </div>
   )
 }
